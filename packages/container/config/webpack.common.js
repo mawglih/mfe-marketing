c25a5a9243a7aed942152d4934102fcfd0bd94dd
Oleg Markoff <mawglih@gmail.com>
2021-12-01 10:48:35 -0500

    created uml

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     module: {
         rules: [
@@ -9,9 +11,14 @@ module.exports = {
                     options: {
                         presets: ['@babel/preset-react', '@babel/preset-env'],
                         plugins: ['@babel/plugin-transform-runtime'],
-                    }
-                }
-            }
-        ]
-    }
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        }),
+    ],
 }
